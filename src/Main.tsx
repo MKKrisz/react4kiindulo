a504004ee352fa5952671a9b48b3756892c4e25c
Egy név <valamilyen@email>
2025-11-10 14:50:56 +0100

    initial

---
+++ src/Main.tsx
@@ -0,0 +1,16 @@
+import "./Pwa"
+import { LeftPane } from "./LeftPane";
+import { RightPane } from "./RightPane";
+import "./Main.css"
+import { useState } from "preact/hooks";
+import { ConversationDto } from "./ChatService";
+
+export function Main()
+{
+	let [ selected, setSelected ] = useState<ConversationDto>();
+
+	return <div class="Main">
+		<LeftPane selected={ selected } onSelect={ setSelected } />
+		<RightPane conversation={ selected } />
+	</div>
+}
